e9bbdde6a36dd7c6e30f3157a7736bfe70a537d9
danny <ceozhangtao@qq.com>
2018-01-04 18:58:47 +0800

    修改表结构

--- doc/lvsen.sql
+++ doc/lvsen.sql
@@ -1,16 +1,16 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : Localhost
-Source Server Version : 50505
+Source Server         : localhost
+Source Server Version : 50628
 Source Host           : localhost:3306
 Source Database       : lvsen
 
 Target Server Type    : MYSQL
-Target Server Version : 50505
+Target Server Version : 50628
 File Encoding         : 65001
 
-Date: 2018-01-03 22:42:10
+Date: 2018-01-04 18:58:21
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -25,7 +25,8 @@ CREATE TABLE `bills` (
   `order_id` bigint(20) DEFAULT NULL COMMENT '单据ID',
   `payable_amount` float(20,0) DEFAULT NULL COMMENT '账单应付金额',
   `paid_amount` float(20,0) DEFAULT NULL COMMENT '已结算金额',
-  `status` tinyint(1) DEFAULT 0 COMMENT '结算状态，0-未完成，1-已完成',
+  `status` tinyint(1) DEFAULT '0' COMMENT '结算状态，0-未完成，1-已完成',
+  `create_time` datetime DEFAULT NULL COMMENT '账单生成时间',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -50,6 +51,7 @@ CREATE TABLE `client` (
   `phone2` varchar(20) DEFAULT NULL,
   `type` varchar(8) DEFAULT '' COMMENT '客户类型(供货商，营销商)',
   `default_price_category` int(8) DEFAULT NULL COMMENT '默认的销售适用价格',
+  `remark` varchar(200) DEFAULT NULL COMMENT '备注信息',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='客户信息';
 
@@ -84,13 +86,13 @@ CREATE TABLE `goods` (
   `acronym` varchar(20) DEFAULT NULL,
   `category_id` int(8) DEFAULT NULL,
   `specification` varchar(32) DEFAULT '' COMMENT '商品规格（例如:1x4x25）',
-  `status` int(4) DEFAULT 1 COMMENT '状态 1:启用，0:禁用',
+  `status` int(4) DEFAULT '1' COMMENT '状态 1:启用，0:禁用',
   `min_price` float(20,0) DEFAULT NULL COMMENT '最低价格',
   `capacity` int(11) DEFAULT NULL,
   `mini_unit` varchar(10) DEFAULT '' COMMENT '最小单位名称',
   `default_unit_id` int(11) DEFAULT NULL COMMENT '默认交易单位',
   `storage_partition_number` varchar(20) DEFAULT '' COMMENT '库位编号',
-  `is_scan` tinyint(1) DEFAULT 1 COMMENT '是否需要扫码',
+  `is_scan` tinyint(1) DEFAULT '1' COMMENT '是否需要扫码',
   `single_code` varchar(36) DEFAULT '' COMMENT '商系条形码编号',
   `weight` float(10,0) DEFAULT NULL COMMENT '重量，单位是千克',
   `volume` float(10,0) DEFAULT NULL COMMENT '体积，单位为立方米',
@@ -143,7 +145,7 @@ CREATE TABLE `goods_category` (
   `pinyin` varchar(50) DEFAULT NULL,
   `acronym` varchar(10) DEFAULT NULL,
   `parent_id` int(11) DEFAULT NULL,
-  `status` tinyint(1) DEFAULT 1 COMMENT '状态: 1-启用，0-禁用',
+  `status` tinyint(1) DEFAULT '1' COMMENT '状态: 1-启用，0-禁用',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -212,7 +214,7 @@ CREATE TABLE `orders` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `number` varchar(32) DEFAULT '' COMMENT '单据编号',
   `code` varchar(30) DEFAULT NULL COMMENT '条形码编号',
-  `create_user` varchar(20) DEFAULT '' COMMENT '制单人',
+  `create_user_id` int(11) DEFAULT NULL COMMENT '制单人ID',
   `way_id` int(8) DEFAULT NULL COMMENT '订单来源途径',
   `create_date` datetime DEFAULT NULL COMMENT '创建日期',
   `delivery_date` datetime DEFAULT NULL COMMENT '交货日期',
@@ -224,11 +226,11 @@ CREATE TABLE `orders` (
   `discount` float(8,0) DEFAULT NULL COMMENT '折扣',
   `digest` varchar(50) DEFAULT '' COMMENT '摘要信息',
   `storage_id` int(11) DEFAULT NULL,
-  `comment` varchar(100) DEFAULT '' COMMENT '备注信息',
+  `remark` varchar(100) DEFAULT '' COMMENT '备注信息',
   `total_money` float(20,0) DEFAULT NULL COMMENT '单据总额',
   `paid_money` float(20,0) DEFAULT NULL COMMENT '已付金额',
-  `is_changed` tinyint(1) DEFAULT 0 COMMENT '是否进行过修改0-未修改,1-被修改过',
-  `print_count` int(8) DEFAULT 1 COMMENT '打印次数',
+  `is_changed` tinyint(1) DEFAULT '0' COMMENT '是否进行过修改0-未修改,1-被修改过',
+  `print_count` int(8) DEFAULT '1' COMMENT '打印次数',
   `pay_status` tinyint(1) DEFAULT NULL COMMENT '结算状态，0-未完成，1-已完成',
   `ext` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`)
@@ -265,14 +267,14 @@ CREATE TABLE `order_goods` (
   `goods_id` int(11) DEFAULT NULL,
   `unit_id` int(11) DEFAULT NULL COMMENT '单位ID',
   `spec` varchar(20) DEFAULT '' COMMENT '规格',
-  `total_number` int(11) DEFAULT 0 COMMENT '单据预期数量',
+  `total_number` int(11) DEFAULT '0' COMMENT '单据预期数量',
   `real_number` int(11) DEFAULT NULL COMMENT '实际数量',
   `unit_tranfer` varchar(32) DEFAULT '' COMMENT '单位换算',
-  `price` float(20,0) DEFAULT 0 COMMENT '价格',
+  `price` float(20,0) DEFAULT '0' COMMENT '价格',
   `discount` float(10,0) DEFAULT NULL COMMENT '折扣',
-  `sum` float(20,0) DEFAULT 0 COMMENT '金额',
-  `discount_price` float(20,0) DEFAULT 0 COMMENT '折后价格',
-  `discount_sum` float(20,0) DEFAULT 0 COMMENT '折后金额',
+  `sum` float(20,0) DEFAULT '0' COMMENT '金额',
+  `discount_price` float(20,0) DEFAULT '0' COMMENT '折后价格',
+  `discount_sum` float(20,0) DEFAULT '0' COMMENT '折后金额',
   `is_largess` varchar(20) DEFAULT '' COMMENT '是否为赠品：1-是,0-不是',
   `storage_id` varchar(255) DEFAULT NULL,
   `comment` varchar(50) DEFAULT '' COMMENT '备注信息',
@@ -327,7 +329,7 @@ CREATE TABLE `storage` (
   `acronym` varchar(10) DEFAULT '' COMMENT '仓库名称缩写',
   `address` varchar(100) DEFAULT NULL,
   `manager` varchar(20) DEFAULT '' COMMENT '仓库管理员',
-  `status` int(4) DEFAULT 1 COMMENT '状态 1:可用，0:禁用',
+  `status` int(4) DEFAULT '1' COMMENT '状态 1:可用，0:禁用',
   `capacity` varchar(20) DEFAULT NULL,
   `area_number` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
@@ -348,13 +350,106 @@ CREATE TABLE `storage_partition` (
   `area` varchar(4) DEFAULT '' COMMENT '区域编号',
   `row` int(4) DEFAULT NULL COMMENT '区域的行号',
   `layer` int(4) DEFAULT NULL COMMENT '层号',
-  `place` int(8) DEFAULT 0 COMMENT '一层的位置',
+  `place` int(8) DEFAULT '0' COMMENT '一层的位置',
   `status` tinyint(1) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='仓库划分';
 
 -- ----------------------------
 -- Records of storage_partition
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for sys_menu
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_menu`;
+CREATE TABLE `sys_menu` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `menu_code` varchar(16) DEFAULT NULL COMMENT '菜单权限码',
+  `menu_name` varchar(20) DEFAULT NULL,
+  `menu_order` int(8) DEFAULT NULL COMMENT '菜单顺序，按升序方式展示',
+  `parent_id` int(11) DEFAULT NULL COMMENT '父菜单ID，顶级菜单默认-1',
+  `menu_url` varchar(50) DEFAULT NULL COMMENT '菜单链接地址，父菜单URL为空',
+  `menu_status` tinyint(1) DEFAULT NULL COMMENT '菜单状态0-禁用，1-启用',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of sys_menu
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for sys_role
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_role`;
+CREATE TABLE `sys_role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `role_name` varchar(20) DEFAULT NULL COMMENT '角色名称',
+  `role_code` varchar(20) DEFAULT NULL COMMENT '角色代号',
+  `role_status` tinyint(1) DEFAULT NULL COMMENT '状态:0-禁用，1-启用',
+  `remark` varchar(200) DEFAULT NULL COMMENT '备注信息',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of sys_role
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for sys_role_privilege
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_role_privilege`;
+CREATE TABLE `sys_role_privilege` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `role_id` int(11) DEFAULT NULL COMMENT '角色ID',
+  `menu_id` int(11) DEFAULT NULL COMMENT '权限ID',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of sys_role_privilege
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for sys_user
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_user`;
+CREATE TABLE `sys_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `account` varchar(20) DEFAULT '' COMMENT '用户帐号',
+  `password` varchar(36) DEFAULT '' COMMENT '登录密码',
+  `name` varchar(20) DEFAULT NULL,
+  `pinyin` varchar(36) DEFAULT '' COMMENT '姓名拼音',
+  `acronym` varchar(10) DEFAULT '' COMMENT '姓名缩写',
+  `sex` tinyint(1) DEFAULT NULL COMMENT '性别 1-男，2-女',
+  `phone` varchar(20) DEFAULT '' COMMENT '联系电话',
+  `birthday` varchar(20) DEFAULT NULL,
+  `score` int(8) DEFAULT NULL COMMENT '权限分值',
+  `job_desc` varchar(64) DEFAULT '' COMMENT '职位描述',
+  `phone2` varchar(20) DEFAULT '',
+  `department` varchar(20) DEFAULT '' COMMENT '所属部门',
+  `status` tinyint(1) DEFAULT '1' COMMENT '状态，0-禁用，1-启用',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='用户信息';
+
+-- ----------------------------
+-- Records of sys_user
+-- ----------------------------
+INSERT INTO `sys_user` VALUES ('1', 'admin', '0192023a7bbd73250516f069df18b500', '管理员', 'chaojiguanliyuan', 'cjgly', null, '', null, '100', '超级管理员', '', '', '1');
+
+-- ----------------------------
+-- Table structure for sys_user_role
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_user_role`;
+CREATE TABLE `sys_user_role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `role_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of sys_user_role
 -- ----------------------------
 
 -- ----------------------------
@@ -366,36 +461,11 @@ CREATE TABLE `unit` (
   `name` varchar(10) DEFAULT '' COMMENT '单位名称',
   `pinyin` varchar(20) DEFAULT NULL,
   `acronym` varchar(10) DEFAULT NULL,
-  `type` int(8) DEFAULT 0 COMMENT '计量单位类型',
+  `type` int(8) DEFAULT '0' COMMENT '计量单位类型',
   `belong` varchar(32) DEFAULT '' COMMENT '单位所属类型',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='计量单位信息';
 
 -- ----------------------------
 -- Records of unit
--- ----------------------------
-
--- ----------------------------
--- Table structure for user
--- ----------------------------
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE `user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `account` varchar(20) DEFAULT '' COMMENT '用户帐号',
-  `password` varchar(20) DEFAULT '' COMMENT '登录密码',
-  `name` varchar(20) DEFAULT NULL,
-  `pinyin` varchar(36) DEFAULT '' COMMENT '姓名拼音',
-  `acronym` varchar(10) DEFAULT '' COMMENT '姓名缩写',
-  `phone` varchar(20) DEFAULT '' COMMENT '联系电话',
-  `birthday` varchar(20) DEFAULT NULL,
-  `score` int(8) DEFAULT NULL COMMENT '权限分值',
-  `job_desc` varchar(64) DEFAULT '' COMMENT '职位描述',
-  `phone2` varchar(20) DEFAULT '',
-  `department` varchar(20) DEFAULT '' COMMENT '所属部门',
-  `status` tinyint(1) DEFAULT 1 COMMENT '状态，0-禁用，1-启用',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户信息';
-
--- ----------------------------
--- Records of user
 -- ----------------------------
